3d36dc65a92cf59c1ecfa2184d423010a1d6147c
oswdev12e74 <akshat.betala@gmail.com>
2026-05-29 15:20:44 +0530

    Added Databases

--- user-service/src/main/java/com/staysphere/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/staysphere/user_service/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.staysphere.user_service;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
