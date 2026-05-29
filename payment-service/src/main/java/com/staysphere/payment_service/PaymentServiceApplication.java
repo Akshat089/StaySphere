78b684d6c12859787a14186d45b8a0abbf53ae69
oswdev12e74 <akshat.betala@gmail.com>
2026-05-29 21:46:57 +0530

    Day 1

--- payment-service/src/main/java/com/staysphere/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/staysphere/payment_service/PaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.staysphere.payment_service;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
 
---
+++ postgres-init/01-create-databases.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE booking_db OWNER staysphere;
+CREATE DATABASE property_db OWNER staysphere;
+CREATE DATABASE payment_db OWNER staysphere;
+CREATE DATABASE review_db OWNER staysphere;
+CREATE DATABASE notification_db OWNER staysphere;
+CREATE DATABASE search_db OWNER staysphere;
+CREATE DATABASE gateway_db OWNER staysphere;
